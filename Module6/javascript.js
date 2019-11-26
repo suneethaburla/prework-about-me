b2e0c3e0250f16fbdf742174684a4c890ca49217
suneethaburla <suneetha@gmail.com>
2019-11-26 12:00:17 -0800

    updated javascript.js

---
+++ Module6/javascript.js
@@ -0,0 +1,22 @@
+
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "50px";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0.5";
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px" ;
+});
